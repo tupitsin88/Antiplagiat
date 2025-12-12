13304fa2d1aaa631358055118dc3c5ec451d68ae
Timofey Tupitsin <trtupitsin@edu.hse.ru>
2025-12-12 15:06:58 +0300

    Add nginx and modify docker

--- file-analysis-service/internal/models/models.go
+++ file-analysis-service/internal/models/models.go
@@ -2,18 +2,12 @@ package models
 
 import "time"
 
-type Work struct {
-	ID             int       `json:"id"`
-	StudentName    string    `json:"student_name"`
-	AssignmentName string    `json:"assignment_name"`
-	UploadedAt     time.Time `json:"uploaded_at"`
-}
-
 type PlagiatReport struct {
 	ID             int       `json:"id"`
 	WorkID         int       `json:"work_id"`
 	PlagiatScore   float32   `json:"plagiat_score"`   // 0.0 - 100.0
-	PlagiatSources string    `json:"plagiat_sources"` // JSON string
+	PlagiatSources string    `json:"plagiat_sources"` // JSON string or text
+	WordCloudURL   string    `json:"word_cloud_url"`  // Ссылка на облако слов
 	CheckedAt      time.Time `json:"checked_at"`
 }
 
@@ -21,4 +15,9 @@ type CheckResponse struct {
 	ReportID     int     `json:"report_id"`
 	PlagiatScore float32 `json:"plagiat_score"`
 	Status       string  `json:"status"` // "checked"
+}
+
+type WorkData struct {
+	ID          int
+	StudentName string
 }
--- file-storing-service/internal/models/models.go
+++ file-storing-service/internal/models/models.go
@@ -6,6 +6,5 @@ type Work struct {
 	ID             int       `json:"id"`
 	StudentName    string    `json:"student_name"`
 	AssignmentName string    `json:"assignment_name"`
-	FileContent    string    `json:"file_content"`
 	UploadedAt     time.Time `json:"uploaded_at"`
 }
